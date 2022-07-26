00c7cd8df9fb20af1e69502a941543beca495eee
raquel-ro <raquelcromero2016@gmail.com>
2022-07-25 22:02:14 -0700

    images in gridview

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'findresources.dart';
 
-void goTofindresources(BuildContext context) {
-  Navigator.push(context, MaterialPageRoute(builder: (context) {
-    return FindResources();
-  }));
-}
-
 void main() {
   runApp(const MyApp());
 }
@@ -18,27 +12,213 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Refu-Get'),
-        ),
-        body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(10.0),
-              alignment: Alignment.topLeft,
-              child:
-                  ElevatedButton(onPressed: () {}, child: const Text('Back')),
+        appBar: AppBar(title: const Text('Refu-Get')),
+        body: new GridView.count(
+          crossAxisCount: 3,
+          children: <Widget>[
+            new Container(
+              child: new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    new Image.asset(
+                      "/Users/raquelromero/myfirstapp/assets/images/hospital.jpeg",
+                      height: 120.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                    new SizedBox(
+                      height: 5.0,
+                    ),
+                    new Text(
+                      "Clinic/Hopsital",
+                      style: TextStyle(fontSize: 20.0, color: Colors.black87),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Container(
-              margin: const EdgeInsets.all(20.0),
-              alignment: Alignment.topCenter,
-              child: const Text('Find What You Need:'),
+            new Container(
+              child: new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    new Image.asset(
+                      "/Users/raquelromero/myfirstapp/assets/images/groceries.png",
+                      height: 120.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                    new SizedBox(
+                      height: 5.0,
+                    ),
+                    new Text(
+                      "Grocery Store",
+                      style: TextStyle(fontSize: 20.0, color: Colors.black87),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Container(
+              child: new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    new Image.asset(
+                      "/Users/raquelromero/myfirstapp/assets/images/school.jpeg",
+                      height: 120.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                    new SizedBox(
+                      height: 5.0,
+                    ),
+                    new Text(
+                      "Schools",
+                      style: TextStyle(fontSize: 20.0, color: Colors.black87),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Container(
+              child: new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    new Image.asset(
+                      "/Users/raquelromero/myfirstapp/assets/images/translationservices.jpeg",
+                      height: 120.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                    new SizedBox(
+                      height: 5.0,
+                    ),
+                    new Text(
+                      "Translation Services",
+                      style: TextStyle(fontSize: 17.0, color: Colors.black87),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Container(
+              child: new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    new Image.asset(
+                      "/Users/raquelromero/myfirstapp/assets/images/restaurants.jpeg",
+                      height: 120.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                    new SizedBox(
+                      height: 5.0,
+                    ),
+                    new Text(
+                      "Restaurants",
+                      style: TextStyle(fontSize: 20.0, color: Colors.black87),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Container(
+              child: new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    new Image.asset(
+                      "/Users/raquelromero/myfirstapp/assets/images/daycarePs.jpeg",
+                      height: 120.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                    new SizedBox(
+                      height: 5.0,
+                    ),
+                    new Text(
+                      "Daycare/Preschool",
+                      style: TextStyle(fontSize: 17.0, color: Colors.black87),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Container(
+              child: new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    new Image.asset(
+                      "/Users/raquelromero/myfirstapp/assets/images/religiousPlaces.jpeg",
+                      height: 120.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                    new SizedBox(
+                      height: 5.0,
+                    ),
+                    new Text(
+                      "Religious Places",
+                      style: TextStyle(fontSize: 20.0, color: Colors.black87),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Container(
+              child: new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    new Image.asset(
+                      "/Users/raquelromero/myfirstapp/assets/images/clothing.png",
+                      height: 120.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                    new SizedBox(
+                      height: 5.0,
+                    ),
+                    new Text(
+                      "Clothing Stores",
+                      style: TextStyle(fontSize: 20.0, color: Colors.black87),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Container(
+              child: new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    new Image.asset(
+                      "/Users/raquelromero/myfirstapp/assets/images/policeft.jpeg",
+                      height: 120.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                    new SizedBox(
+                      height: 5.0,
+                    ),
+                    new Text(
+                      "Police/Fire Stations",
+                      style: TextStyle(fontSize: 17.0, color: Colors.black87),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
-            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.today)),
+            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
             BottomNavigationBarItem(label: 'To Do', icon: Icon(Icons.task)),
             BottomNavigationBarItem(
                 label: 'Translate', icon: Icon(Icons.translate))
---
+++ lib/welcomepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Welcomepage extends StatefulWidget {
+  const Welcomepage({Key? key}) : super(key: key);
+
+  @override
+  State<Welcomepage> createState() => _WelcomepageState();
+}
+
+class _WelcomepageState extends State<Welcomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+      title: Text(''),
+    ));
+  }
+}
